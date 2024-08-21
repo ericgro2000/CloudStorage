ab3cc51e26dec3e5e0700089463d33c12e61740f
ericgro2000 <eric.gro.2000@gmail.com>
2024-08-21 16:33:51 +0200

    fixed jpeg bug

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './navbar.scss'
-//import Logo from '../assets/imgs/cloudyimg.jpeg'
+// import './navbar.css'
+import Logo from './clodyimg.jpeg'
 import {NavLink} from "react-router-dom";
 const Navbar = () => {
     return (
         <div className="navbar">
             <div className="container">
-                {/* <img src={Logo} alt="" className="navbar__logo"/> */}
+                <img src={Logo} alt="" className="navbar__logo"/>
                 <div className="navbar__header">Cloudy</div>
                 <div className="navbar__login"><NavLink to="/login">Login</NavLink></div>
                 <div className="navbar__registration"><NavLink to="/registration">Registration</NavLink></div>
