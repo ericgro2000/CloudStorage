c667f330a79a406e09c2e5932635a286b646baa5
Erik <147524279+ericgro2000@users.noreply.github.com>
2024-08-22 15:01:07 +0000

    add alert when registration was successful

--- client/src/api/registration.ts
+++ client/src/api/registration.ts
@@ -2,12 +2,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://q5xqc2-5000.csb.app/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://q5xqc2-7000.csb.app/" }),
   endpoints: (builder) => ({
     registration: builder.mutation<string, { email: string; password: string }>(
       {
         query: (credentials) => ({
-          url: "/auth/registration",
+          url: "api/auth/registration",
           method: "POST",
           body: credentials,
         }),
--- client/src/components/registration/Registration.tsx
+++ client/src/components/registration/Registration.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./registration.css";
 import Input from "utils/input/Input";
 import { useRegistrationMutation } from "api/registration";
@@ -6,7 +6,15 @@ import { useRegistrationMutation } from "api/registration";
 const Registration = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [register, { isLoading }] = useRegistrationMutation();
+  const [register, { isLoading, isSuccess }] = useRegistrationMutation();
+
+  useEffect(() => {
+    console.log(isSuccess);
+    if (isSuccess) {
+      // Redirect to another route after a successful fetch
+      alert("happily loggined!");
+    }
+  }, [isSuccess]);
 
   return (
     <div className="registration">
