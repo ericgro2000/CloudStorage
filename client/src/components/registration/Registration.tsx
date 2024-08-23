7eca48872a268369b4d1eae75bb82fe8278461cb
Erik <147524279+ericgro2000@users.noreply.github.com>
2024-08-23 13:21:54 +0000

    started to work to make a form reusable component for registration and login

--- client/src/components/registration/Registration.tsx
+++ client/src/components/registration/Registration.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./registration.css";
 import Input from "utils/input/Input";
 import { useRegistrationMutation } from "api/registration";
+import Form from "utils/form/form";
 
 const Registration = () => {
   const [email, setEmail] = useState("");
@@ -18,25 +19,14 @@ const Registration = () => {
 
   return (
     <div className="registration">
-      <div className="registration__header">Регистрация</div>
-      <Input
-        value={email}
-        setValue={setEmail}
-        type="text"
-        placeholder="Введите email..."
+      <Form
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        register={register}
+        isLoading={isLoading}
       />
-      <Input
-        value={password}
-        setValue={setPassword}
-        type="password"
-        placeholder="Введите пароль..."
-      />
-      <button
-        className="registration__btn"
-        onClick={() => register({ email, password })}
-      >
-        Войти
-      </button>
     </div>
   );
 };
